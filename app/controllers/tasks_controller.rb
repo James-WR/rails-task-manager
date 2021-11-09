10317dc45bb261845c258d713d979c8758fbbc1e
James Robbs <jamesrobbs01@gmail.com>
2021-11-09 16:21:32 +0000

    implemented task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,23 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @new_task = Task.new
+  end
+
+  def create
+    @created_task = Task.new(task_params)
+    @created_task.save
+    redirect_to banana_path(@created_task)
+  end
+
+  def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :banana
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit'
 end
